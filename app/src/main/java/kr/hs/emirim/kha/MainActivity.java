3003d8de58b3f9675ff046cd37a1f35e627d1017
joyun <w2039@e-mirim.hs.kr>
2021-08-26 20:48:33 +0900

    Edit actviy_main.xml

--- app/src/main/java/kr/hs/emirim/kha/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/kha/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,15 +14,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        View.OnClickListener btnHomeListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        };
+        Button btn = findViewById(R.id.btn1);
+        btn.setOnClickListener(btnListener);
+
     }
+
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(getApplicationContext(),MainActivity2.class);
+            startActivity(intent);
+            finish();
+        }
+    };
 
 
 }
